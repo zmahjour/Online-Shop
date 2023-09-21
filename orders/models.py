62bbe39de0563059f9c9d83de8a1c7f6e82eadb9
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-21 21:14:14 +0330

    chore: define Order model

--- orders/models.py
+++ orders/models.py
@@ -3,3 +3,26 @@ from accounts.models import User
 from products.models import Product
 
 
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "Pending"
+        PROCESSING = "Processing"
+        SENDING = "Sending"
+        CANCELLED = "Cancelled"
+        DELIVERED = "Delivered"
+
+    customer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="orders"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=Status.choices)
+    is_paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["is_paid", "-updated"]
+
+    def __str__(self):
+        return f"order: {self.id} | status: {self.status}"
+
+
