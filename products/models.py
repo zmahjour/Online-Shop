2354b422225ea061148e8df77b075bf1693c4103
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-21 21:15:00 +0330

    chore: define Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    sub_category = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+
