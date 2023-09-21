be2d41080af3c19260f620866fe1811dbdc80cb8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-21 21:15:19 +0330

    chore: define Product model

--- products/models.py
+++ products/models.py
@@ -16,3 +16,18 @@ class Category(models.Model):
         return self.title
 
 
+class Product(models.Model):
+    category = models.ManyToManyField(Category, related_name="products")
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
