bc749191fcd88312d694df10d82cc8e79fb58da2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-21 21:14:31 +0330

    chore: define OrderItem model

--- orders/models.py
+++ orders/models.py
@@ -26,3 +26,11 @@ class Order(models.Model):
         return f"order: {self.id} | status: {self.status}"
 
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.id}"
