1d1779d101466eb93c4bb1d9202b866f76c21fa1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-06 11:42:57 +0330

    chore: define set_otp function

--- core/utils.py
+++ core/utils.py
@@ -8,3 +8,7 @@ def send_otp_code(phone_number, code):
     print("*" * 120)
 
 
+def set_otp(phone_number):
+    random_code = random.randint(1000, 9999)
+    send_otp_code(phone_number=phone_number, code=random_code)
+    return random_code
