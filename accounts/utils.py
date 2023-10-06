05a66c99783025068e3c28c4faa1ded6448da359
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-06 11:44:42 +0330

    chore: define JWTToken class with two methods for generating access and refresh token

--- accounts/utils.py
+++ accounts/utils.py
@@ -4,3 +4,31 @@ import jwt
 import datetime
 
 
+class JWTToken:
+    jti = str(uuid.uuid4())
+
+    def generate_access_token(self, user):
+        access_token_payload = {
+            "user_id": user.id,
+            "exp": datetime.datetime.now() + settings.ACCESS_EXPIRE_TIME,
+            "iat": datetime.datetime.now(),
+            "jti": self.jti,
+        }
+
+        access_token = jwt.encode(
+            access_token_payload, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return access_token
+
+    def generate_refresh_token(self, user):
+        refresh_token_payload = {
+            "user_id": user.id,
+            "exp": datetime.datetime.now() + settings.REFRESH_EXPIRE_TIME,
+            "iat": datetime.datetime.now(),
+            "jti": self.jti,
+        }
+
+        refresh_token = jwt.encode(
+            refresh_token_payload, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return refresh_token
