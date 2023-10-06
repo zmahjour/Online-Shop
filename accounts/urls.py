eed754840b1cfc5d451e27da4723b8e55431fce8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-06 11:55:53 +0330

    chore: define urls for register, login and verify

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
+    path("register/", views.UserRegisterView.as_view()),
+    path("login/", views.UserLoginView.as_view()),
+    path("verify/", views.UserVerificationView.as_view())
+    # path("logout/", views.UserLogoutView.as_view()),
+    # path("token/access/", views.ObtainAccessTokenView.as_view()),
 ]
