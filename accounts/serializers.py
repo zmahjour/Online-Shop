7f3b07564c5a4e92e689a0555f28954e7cfeacb6
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-06 11:52:31 +0330

    chore: define UserRegisterSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 from .models import User
 
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["phone_number", "full_name"]
+
