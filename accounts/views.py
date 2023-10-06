6f2f9e391c21330195da70900edb677261174a13
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-06 12:00:14 +0330

    chore: define UserLoginView

--- accounts/views.py
+++ accounts/views.py
@@ -25,3 +25,24 @@ class UserRegisterView(APIView):
             return Response(data=serialized_data.data, status=status.HTTP_201_CREATED)
 
 
+class UserLoginView(APIView):
+    authentication_classes = []
+
+    def post(self, request, *args, **kwargs):
+        serialized_data = UserLoginSerializer(data=request.data)
+        if serialized_data.is_valid(raise_exception=True):
+            phone_number = serialized_data.validated_data.get("phone_number")
+            user = User.objects.filter(phone_number=phone_number).exists()
+
+            if not user:
+                return Response(
+                    {"message": "Any user does not exist with this phone number."},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+            random_code = set_otp(phone_number=phone_number)
+            cache.set(key=phone_number, value=random_code, timeout=120)
+            request.session["phone_number"] = phone_number
+            return Response({"message": "Verification code has been sent to you."})
+
+
